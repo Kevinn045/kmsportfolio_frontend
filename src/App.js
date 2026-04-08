34e8efd27ccd0a5de7dd34c8db65c4392be536f5
muse <kevinmuse45@gmail.com>
2026-04-08 19:09:12 +0300

    update frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Dashboard from "./components/dashboard";
 
 
 
@@ -21,6 +22,7 @@ function App() {
       <Header />
       <Projects />
       <Contact />
+      <Dashboard />
     </div>
   );
 }
