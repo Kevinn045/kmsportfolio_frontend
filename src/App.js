38739b720084fa3cfad1b0ef89b8f69766bb39da
muse <kevinmuse45@gmail.com>
2026-04-08 21:54:55 +0300

    update fr

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import AddProject from "./Pages/AddProject";
-import AddProject from "./Pages/Login";
+import Login from "./Pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
