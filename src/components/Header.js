17a5697fb9fce5c703c0a962c6b5abdf434b269f
muse <kevinmuse45@gmail.com>
2026-04-08 19:30:19 +0300

    force redeploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,8 @@
 function Header() {
     return (
         <div className="bg-light text-center p-5">
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-            >
-                <h1>Hi, I'm Elite Dev</h1>
-            </motion.div>
-
+            <h1>Hi,</h1>
+            <p>Full Stack Developer (Django + React)</p>
         </div>
     );
 }
