d99e1c54ff24b1cf485e9353bc12fd0bcead5278
Kevinn045 <98132402+Kevinn045@users.noreply.github.com>
2026-07-14 13:36:50 +0300

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
 
         try {
             const res = await axios.post(
-                "https://kmsportfolio-back.onrender.com/api/token/",
+                "https://kmsportfolio-back.onrender.com/api/login/",
                 formData
             );
 
