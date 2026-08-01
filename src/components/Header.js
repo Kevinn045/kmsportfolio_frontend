1d90d2278c24bb63e7a03ffd5fc6a3bd0e00dc3d
Kevin Muse <kevinmuse45@gmail.com>
2026-08-01 11:13:22 +0300

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
@@ -66,7 +66,7 @@ function Header() {
 
             <div className="code-window">
               <span className="code-comment">
-                // What I enjoy building
+                {'//what I enjoy building'}
               </span>
 
               <span>
